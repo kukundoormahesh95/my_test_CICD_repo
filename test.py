08a6d2a48f082cc29dfc063e7b18adf4cd1005b3
kukundoormahesh95 <60536242+kukundoormahesh95@users.noreply.github.com>
2025-10-25 12:54:58 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import pytest
+from app import app
+@pytest.fixture
+def client():
+  app.config['Testing'] = True
+  with app.test_client() as client:
+     yield client
+
+def test_app_is_working(client):
+  response=client.get("/")
+  assert response.status_code==200
